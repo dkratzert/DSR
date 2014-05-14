a06af14a8fd232f9ded1097394ebd7247a012dba
Daniel Kratzert <dkratzert@gmx.de>
2014-05-14 13:13:23 +0200

    build script

--- make_release.sh
+++ make_release.sh
@@ -8,7 +8,10 @@ VERSION=$(cat $GIT/dsr.py|grep -e "VERSION ="|cut -d ' ' -f3|tr -d "\'")
 
 cd $GIT
 
-git pull
+cd setup
+sh build_linux_distrib.sh
+
+cd $GIT
 
 cp $GIT/setup/dsr-linux.spec $PACK/SPECS
 
@@ -16,7 +19,7 @@ cd $PACK/SPECS
 
 rpmbuild -bb dsr-linux.spec
 
-sh setup/build_linux_distrib.sh
+cd $GIT
 
 cp -r setup/debian-package/DEBIAN $PACK/BUILD/dsr
 
@@ -26,3 +29,4 @@ dpkg-deb --build dsr
 
 mv dsr.deb dsr-$VERSION.deb 
 
+cp /usr/src/packages/RPMS/noarch/DSR-$VERSION-0.noarch.rpm /usr/src/packages/BUILD/
