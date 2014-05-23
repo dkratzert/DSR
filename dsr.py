c41c72278aee42f5fb549447e0eeaf0099d06e2c
Daniel Kratzert <dkratzert@gmx.de>
2014-05-23 14:51:08 +0200

    finding dsr command fixes

--- dsr.py
+++ dsr.py
@@ -39,7 +39,7 @@ from restraints import Restraints, Adjacency_Matrix
 # -debian package: /usr/src/packages/BUILD # dpkg-deb --build dsr
 
 
-VERSION = '1.5.2'
+VERSION = '1.5.3'
 progname = '\n-----------------------------'\
            ' D S R - v{}' \
            ' ----------------------------------'.format(VERSION)
