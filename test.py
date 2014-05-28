cf5b90631c8f02fa25fbe5a4d07162ddb32162be
Daniel Kratzert <dkratzert@gmx.de>
2014-05-28 13:44:56 +0200

    unit tests

--- test.py
+++ test.py
@@ -4,8 +4,9 @@
 
 import sys, os
 import unittest
-from atomhandling import get_atomtypes, FindAtoms
+from atomhandling import get_atomtypes, FindAtoms, check_source_target
 from resfile import ResList
+from msilib.schema import SelfReg
 
 
 
@@ -72,6 +73,7 @@ class reslistTest(unittest.TestCase):
         self.assertEqual(self.fa.get_atoms_resinumber('C1_0'), '0')
         self.assertEqual(self.fa.get_atoms_resinumber('C1'), '0')
 
+
 class collect_residuesTest(unittest.TestCase):
     def setUp(self):
         self.res_file = 'unit-tests/collect_resi.res'
@@ -81,11 +83,74 @@ class collect_residuesTest(unittest.TestCase):
 
     def testrun_collect_residues(self):
         #print(self.fa.collect_residues())
-        collected_resi = {'1': [['O1', ['0.584527', '0.749093', '0.406892'], 19, 'CCF3'],
-                                ['C1', ['0.462797', '0.766414', '0.415951'], 21, 'CCF3']], '0': []}
+        collected_resi = {'0': [['FE1', ['0.100001', '0.200002', '0.300003'], 19, None]],
+                          '1': [['O1', ['0.584527', '0.749093', '0.406892'], 34, 'CCF3'],
+                                ['H2', ['0.2', '0.3', '0.4'], 36, 'CCF3'],
+                                ['C1', ['0.462797', '0.766414', '0.415951'], 37, 'CCF3'],
+                                ['H1', ['0.2', '0.3', '0.4'], 40, 'CCF3']]}
+        resi_null = [['FE1', ['0.100001', '0.200002', '0.300003'], 19, None]]
+        resi_fe = 'FE1'
         self.assertEqual(self.fa.collect_residues(), collected_resi)
+        self.assertEqual(self.fa.collect_residues()['0'], resi_null)
+        self.assertEqual(self.fa.collect_residues()['0'][0][0], resi_fe)
 
+    def testrun_get_atomcoordinates(self):
+        atoms_fe = ['Fe1']
+        atoms_fe_0 = ['Fe1_0']
+        atoms = ['Fe1', 'C1_1', 'O1_1']
+        fe_upper = 'FE1'
+        fe = {'FE1': ['0.100001', '0.200002', '0.300003']}
+        fe_float = {'FE1': [0.100001, 0.200002, 0.300003]}
+        coords = ['0.100001', '0.200002', '0.300003']
+        atoms_coords = {'C1_1': ['0.462797', '0.766414', '0.415951'],
+                        'O1_1': ['0.584527', '0.749093', '0.406892'],
+                        'FE1': ['0.100001', '0.200002', '0.300003']}
 
+        self.assertEqual(self.fa.get_atomcoordinates(atoms_fe), fe)
+        self.assertNotEqual(self.fa.get_atomcoordinates(atoms_fe), fe_float)
+        self.assertEqual(self.fa.get_atomcoordinates(atoms_fe).keys()[0], fe_upper)
+        self.assertEqual(self.fa.get_atomcoordinates(atoms_fe_0)['FE1_0'], coords)
+        self.assertEqual(self.fa.get_atomcoordinates(atoms), atoms_coords)
+        with self.assertRaises(KeyError):
+            # raises KeyError, because FE1 is in residue 0
+            self.fa.get_atomcoordinates(atoms_fe_0)['FE1_1']
+
+    def testrun_get_atom_line_number(self):
+        line_str = ['34']
+        line_int = [34]
+        self.assertNotEqual(self.fa.get_atom_line_numbers(['O1_1']), line_str)
+        self.assertEqual(self.fa.get_atom_line_numbers(['O1_1']), line_int)
+        self.assertEqual(self.fa.get_atom_line_numbers(['Fe1_0', 'C1_1']), [19, 37])
+        self.assertEqual(self.fa.get_atom_line_numbers(['C1_1', 'Fe1_0']), [37, 19])
+
+    def testrun_check_source_target(self):
+        source = ['C1', 'C3', 'O1', 'F1']
+        source_fail = ['C15', 'C3', 'O1', 'F1']
+        target = ['q1', 'C3', 'O1_2', 'F1']
+        target_num = ['q1', 'C3', 'O1_2']
+        dbatoms = ['C1', 'C2', 'C3', 'F1', 'F2', 'O1']
+        self.assertEqual(check_source_target(source, target, dbatoms), True)
+        self.assertEqual(check_source_target(source, target_num, dbatoms), False)
+        self.assertEqual(check_source_target(source_fail, target, dbatoms), False)
+
+class remove_hydrogenTest(unittest.TestCase):
+    def setUp(self):
+        self.res_file = 'unit-tests/collect_resi.res'
+        self.res_list = ResList(self.res_file)
+        self.reslist =  self.res_list.get_res_list()
+        self.fa = FindAtoms(self.reslist)
+
+    def not_working_testrun_remove_adjacent_hydrogens(self):
+        line1 = 'C1    1    0.462797    0.766414    0.415951    21.00000    0.01038    0.01899 =\n'
+        line2 = '\n'
+        sfac_table = ['C', 'O', 'F', 'AL', 'F', 'H']
+        #print(self.reslist[37])
+        self.fa.remove_adjacent_hydrogens(['O1_1', 'C1_1'], sfac_table)
+        self.fa = FindAtoms(self.reslist)
+        # for i in self.reslist:
+        #     print(i.strip('\n'))
+        self.assertEqual(self.reslist[36], line1)
+        self.assertEqual(self.reslist[39], line2)
 
 if __name__ == "__main__":
     unittest.main()
