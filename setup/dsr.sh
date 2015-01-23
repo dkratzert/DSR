3dab881ae9633c4eb856de237723117a0fcd095a
Daniel Kratzert <dkratzert@gmx.de>
2015-01-23 16:29:04 +0100

    improve dsr startup and environment

--- setup/dsr.sh
+++ setup/dsr.sh
@@ -2,14 +2,15 @@
 
 # initialize DSR_DIR to default directory if not set properly
 if [ ! $DSR_DIR ]; then 
-    DSR_DIR=/opt/DSR
+    export DSR_DIR=/opt/DSR
 fi
 
 if [ ! $DSR_DB_DIR ]; then 
-    DSR_DB_DIR=/opt/DSR
+    export DSR_DB_DIR=/opt/DSR
 fi
 
 PATH=$PATH:$DSR_DIR
-DSR_DB_DIR=$DSR_DB_DIR
+DSR_DIR=$DSR_DB_DIR
 export PATH
 export DSR_DB_DIR
+export DSR_DIR
