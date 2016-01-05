2434500e04cdfa96e688519082b616252a20d57f
Daniel Kratzert <dkratzert@gmx.de>
2016-01-05 11:37:55 +0100

    changed version format

--- dsr.py
+++ dsr.py
@@ -33,7 +33,7 @@ import resfile
 from cf3fit import CF3
 
 
-VERSION = '1.8.0'
+VERSION = '180'
 # dont forget to change version in Innoscript file, spec file and deb file.
 
 program_name = '\n'+((width//2)-9)*'-'+\
