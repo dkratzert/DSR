38690585fda144961b8581d91c40da12f7e3fbd6
Daniel Kratzert <dkratzert@gmx.de>
2016-02-08 13:01:33 +0100

    todo

--- dsr.py
+++ dsr.py
@@ -42,6 +42,10 @@ program_name = '\n'+((width//2)-9)*'-'+\
 
 # TODO and ideas:
 '''
+- If the res file is empty, ShelXle opens the corresponding .ins file!
+  This produces problems with DSR plugin. DSR then runs on an empty file and can not find the 
+  needed parameters and crashes while searching for SFAC. Therefore, DSR should test for an empty 
+  res file!  
 
 '''
 
