8de3f460945ef273a6b6a9e9900e6ce108c6b6e4
Daniel Kratzert <dkratzert@gmx.de>
2016-02-27 18:31:30 +0100

    foo

--- dsr.py
+++ dsr.py
@@ -43,7 +43,7 @@ program_name = '\n'+((width//2)-9)*'-'+\
 
 # TODO and ideas:
 '''
-- Handle non-existing userdb
+- DB tests are broken!
 - If the res file is empty, ShelXle opens the corresponding .ins file!
   This produces problems with DSR plugin. DSR then runs on an empty file and can not find the 
   needed parameters and crashes while searching for SFAC. Therefore, DSR should test for an empty 
