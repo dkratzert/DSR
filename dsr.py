6c4872fc4194566993ee1e7283656e129eeb8c80
Daniel Kratzert <dkratzert@gmx.de>
2016-02-29 18:46:39 +0100

    todo

--- dsr.py
+++ dsr.py
@@ -44,6 +44,7 @@ program_name = '\n'+((width//2)-9)*'-'+\
 # TODO and ideas:
 '''
 - DB tests are broken!
+- Atoms with C1_5 for example from DSR plugin damage the db.
 - If the res file is empty, ShelXle opens the corresponding .ins file!
   This produces problems with DSR plugin. DSR then runs on an empty file and can not find the 
   needed parameters and crashes while searching for SFAC. Therefore, DSR should test for an empty 
