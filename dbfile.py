7d377cb7aae8bd70d1e99f131a7fab445e2c0a0e
Daniel Kratzert <dkratzert@gmx.de>
2016-03-04 13:19:53 +0100

    improved sadi test

--- dbfile.py
+++ dbfile.py
@@ -546,13 +546,18 @@ class global_DB():
         restraints = deepcopy(restr)
         atnames = [i[0].upper() for i in atoms]
         for num, line in enumerate(restraints):
+            prefixes = []
+            dev = 0.02
             line=line.split()
             if not line:
                 continue
             if line[0].upper() == 'SADI':
+                prefixes.append(line[0])
                 del line[0]
                 try:
                     if not str(line[0][0]).isalpha():
+                        prefixes.append(line[0])
+                        dev = line[0]
                         del line[0] # delete standard deviation
                 except(IndexError):
                     return
@@ -574,10 +579,15 @@ class global_DB():
                     b = [float(y) for y in b]
                     dist = atomic_distance(a, b, self.get_unit_cell(fragment))
                     distances.append(dist)
-                stdev = std_dev(distances)
+                dist_minus_longest = sorted(distances)
+                if len(distances) > 2:
+                    del dist_minus_longest[-1]
+                if len(distances) > 5:
+                    del dist_minus_longest[-1]
+                stdev = std_dev(dist_minus_longest)
                 #print(stdev, fragment, stdev*len(pairs))
                 # only do outlier test if standard deviation is suspiciously large:
-                if stdev > 0.07:
+                if stdev > 0.048:
                     outliers = nalimov_test(distances)
                     if outliers:
                         print("\nFragment {}:".format(fragment))
@@ -586,6 +596,10 @@ class global_DB():
                             print('Suspicious deviation in atom pair "{}" ({:4.3f} A, median: {:4.3f}) of SADI line {}:'.format(pair, distances[x], median(distances), num+1))
                             print(restr[num][:60], '...')
                             return False
+                if stdev > 3*float(dev):
+                    print("\nFragment {}:".format(fragment))
+                    print('Suspicious restraints in SADI line {} with high Stdeviation {:4.3f} (median length: {:4.3f} A).'.format(num+1, stdev, median(distances)))
+                    print(' '.join(prefixes+line))
         return True
                         
     
