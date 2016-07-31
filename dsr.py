5e3538a6963d5aa6a7e61523a0e366bae87a51d8
Daniel Kratzert <dkratzert@gmx.de>
2016-07-31 19:29:10 +0200

    add global logger

--- dsr.py
+++ dsr.py
@@ -477,9 +477,36 @@ if __name__ == '__main__':
     cp = cProfile.Profile()
     cp.enable(subcalls=True, builtins=True)
     """
+
+
+    class LoggerWriter:
+        def __init__(self, level):
+            # self.level is really like using log.debug(message)
+            # at least in my case
+            self.level = level
+
+        def write(self, message):
+            # if statement reduces the amount of newlines that are
+            # printed to the logger
+            if message != '\n':
+                self.level(message)
+
+        def flush(self):
+            # create a flush method so things can be flushed when
+            # the system wants to. Not sure if simply 'printing'
+            # sys.stderr is the correct way to do it, but it seemed
+            # to work properly for me.
+            self.level(sys.stderr)
+
     try:
         remove_file(reportlog)
         #dsr = DSR(res_file_name="p21c.res")
+        import logging
+        logging.basicConfig(filename="./dsr.lst", filemode='w', level=logging.INFO)
+        log = logging.getLogger('dsr')
+        log.addHandler(logging.StreamHandler())
+        sys.stdout = LoggerWriter(log.info)
+        sys.stderr = LoggerWriter(log.info)
         dsr = DSR()
     except Exception as e:
         import platform
