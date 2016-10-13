41ca82071fb4520f2d97647ef94a39d6f957eac9
Daniel Kratzert <dkratzert@gmx.de>
2016-10-13 15:32:07 +0200

    missing exit

--- selfupdate.py
+++ selfupdate.py
@@ -179,6 +179,7 @@ def get_update_package(version):
         overwrite_dir(os.path.join(tmpdir, "DSR-{}".format(version)), dsrdir, move=False)
     except OSError:
         print('Unable to perform update. Please run me with super-user rights, e.g.: "sudo DSR_DIR=/opt/DSR /opt/DSR/dsr -u"')
+        sys.exit()
     shutil.rmtree(tmpdir, ignore_errors=True)  # cleanup the files
     post_update_things()
     return True
