8c8e0bc4bcb7dd5bac27e964fc60159a10f28fc6
Daniel Kratzert <dkratzert@gmx.de>
2016-10-25 10:52:26 +0200

    manual update and options cleanup

--- options.py
+++ options.py
@@ -20,7 +20,7 @@ from constants import sep_line
 
 try:
     from argparse import RawTextHelpFormatter
-except(ImportError):
+except ImportError:
     print('\nYour Python version is incompatible with DSR! \nPlease use Python 2.7 or above.\n')
     sys.exit()
 from argparse import ArgumentParser, SUPPRESS
@@ -31,13 +31,55 @@ __metaclass__ = type  # use new-style classes
 
 
 class OptionsParser():
-    '''
+    """
     This class uses the ArgumentParser module to parse the command line options.
-    '''
+    """
     def __init__(self, versionline):
         self.versionline = versionline
-        self._options = self.parse_commandline()
-        return
+        self.parser = ArgumentParser(prog='dsr', formatter_class=RawTextHelpFormatter,
+                                     description='{}\nDisordered Structure Refinement (DSR)\n'.format(self.versionline)
+                                     + '\nExample DSR .res file command line:'
+                                     + '\nREM DSR PUT/REPLACE "Fragment" WITH C1 C2 C3 ON Q1 Q2 Q3 PART 1 OCC -21 ='
+                                     + '\n  RESI DFIX\n'
+                                     + sep_line+'\n'
+                                     + '   PUT:     Just put the fragment source atoms here.\n'
+                                     + '   REPLACE: Replace atoms of PART 0 in 1.3 A distance around target atoms.\n'
+                                     + sep_line
+                                     )
+        self.parser.add_argument("-r", dest="res_file", metavar='"res file"', nargs='+',
+                                 help="res file with DSR command", default=False)
+        self.parser.add_argument("-re", dest="external_restr", metavar='"res file"', nargs='+',
+                                 help="res file with DSR command (write restraints to external file)", default=False)
+        self.parser.add_argument("-e", dest="export_fragment", metavar='"fragment"',
+                                 help="export fragment as .res/.png file", default=False)
+        self.parser.add_argument("-c", dest="export_clip", metavar='"fragment"',
+                                 help="export fragment to clipboard", default=False)
+        self.parser.add_argument("-t", dest="invert", action='store_true',
+                                 help="inverts the current fragment", default=False)
+        self.parser.add_argument("-i", dest="import_grade", metavar='"tgz file"',
+                                 help="import a fragment from GRADE (needs .tgz file)", default=False)
+        self.parser.add_argument("-l", dest="list_db", action="store_true",
+                                 help="list names of all database entries", default=False)
+        self.parser.add_argument("-s", dest="search_string", metavar='"string"',
+                                 help="search the database for a name", default=False)
+        self.parser.add_argument("-g", dest="rigid_group", help="keep group rigid (no restraints)",
+                                 action="store_true", default=False)
+        self.parser.add_argument("-u", dest="selfupdate", help="Update DSR to the most current version",
+                                 action="store_true", default=False)
+        self.parser.add_argument("-ea", dest="export_all", action='store_true',
+                                 help=SUPPRESS, default=False)
+        self.parser.add_argument("-lc", dest="list_db_csv", action='store_true',
+                                 help=SUPPRESS, default=False)
+        self.parser.add_argument("-x", dest="search_extern",
+                                 help=SUPPRESS, default=False)
+        self.parser.add_argument("-ah", dest="head_for_gui",
+                                 help=SUPPRESS, default=False)
+        # with nargs='+' it accepts space in path and returns a list:
+        self.parser.add_argument("-shx", dest="shelxl_ex", nargs='+',
+                                 help=SUPPRESS, default=False)
+        self.parser.add_argument("-n", dest="no_refine", action="store_true",
+                                 help="do not refine after fragment transfer", default=False)
+        self._options = self.parser.parse_args()
 
     def error(self):
         self.parser.print_help()
@@ -45,7 +87,7 @@ class OptionsParser():
 
     @property
     def res_file(self):
-        if self._options.res_file != False:
+        if self._options.res_file:
             rpath = r' '.join(self._options.res_file)
         else:
             return False
@@ -57,7 +99,7 @@ class OptionsParser():
 
     @property
     def external_restr(self):
-        if self._options.external_restr != False:
+        if self._options.external_restr:
             erpath = r' '.join(self._options.external_restr)
         else:
             return False
@@ -120,7 +162,7 @@ class OptionsParser():
         Option to define the path of the shelxl executable
         '''
         spath = ''
-        if self._options.shelxl_ex != False:
+        if self._options.shelxl_ex:
             spath = r' '.join(self._options.shelxl_ex)
         else:
             return False
@@ -161,57 +203,6 @@ class OptionsParser():
     @property
     def all_options(self):
         return self._options
-
-
-
-    def parse_commandline(self):
-        '''parses the command line options and returns
-           the command line options as dict'''
-        self.parser = ArgumentParser(prog='dsr', formatter_class=RawTextHelpFormatter,
-            description='{}\nDisordered Structure Refinement (DSR)\n'.format(self.versionline)
-                + '\nExample DSR .res file command line:\n'
-                + '\nREM DSR PUT/REPLACE "Fragment" WITH C1 C2 C3 ON Q1 Q2 Q3 PART 1 OCC -21 ='
-                + '\n  RESI DFIX\n'
-                + sep_line+'\n'
-                + '   PUT:     Just put the fragment source atoms here.\n'
-                + '   REPLACE: Replace atoms of PART 0 in 1.35 A distance around target atoms.\n'
-                + sep_line
-            )
-        self.parser.add_argument("-r", dest="res_file", metavar='"res file"', nargs='+',\
-                                help="res file with DSR command", default=False)
-        self.parser.add_argument("-re", dest="external_restr", metavar='"res file"', nargs='+',\
-                                help="res file with DSR command (write restraints to external file)", default=False)
-        self.parser.add_argument("-e", dest="export_fragment", metavar='"fragment"', \
-                                help="export fragment as .res/.png file", default=False)
-        self.parser.add_argument("-c", dest="export_clip", metavar='"fragment"', \
-                                help="export fragment to clipboard", default=False)
-        self.parser.add_argument("-t", dest="invert", action='store_true', \
-                                help="inverts the current fragment", default=False)
-        self.parser.add_argument("-i", dest="import_grade", metavar='"tgz file"', \
-                                help="import a fragment from GRADE (needs .tgz file)", default=False)
-        self.parser.add_argument("-l", dest="list_db", action="store_true", \
-                                help="list names of all database entries", default=False)
-        self.parser.add_argument("-s", dest="search_string", metavar='"string"', \
-                                help="search the database for a name", default=False)
-        self.parser.add_argument("-g", dest="rigid_group", help="keep group rigid (no restraints)", \
-                                action="store_true", default=False)
-        self.parser.add_argument("-u", dest="selfupdate", help="Update DSR to the most current version", \
-                                 action="store_true", default=False)
-        self.parser.add_argument("-ea", dest="export_all", action='store_true', \
-                                help=SUPPRESS, default=False)
-        self.parser.add_argument("-lc", dest="list_db_csv", action='store_true', \
-                                help=SUPPRESS, default=False)
-        self.parser.add_argument("-x", dest="search_extern", \
-                                help=SUPPRESS, default=False)
-        self.parser.add_argument("-ah", dest="head_for_gui", \
-                                help=SUPPRESS, default=False)
-                                # with nargs='+' it accepts space in path and returns a list:
-        self.parser.add_argument("-shx", dest="shelxl_ex", nargs='+',\
-                                 help=SUPPRESS, default=False)
-        self.parser.add_argument("-n", dest="no_refine", action="store_true", \
-                                help="do not refine after fragment transfer", default=False)
-        return self.parser.parse_args()
-
 
 
 
