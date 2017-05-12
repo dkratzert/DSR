5097ce246c05a6a46f4959b14e8f973cc4ebfa39
Daniel Kratzert <dkratzert@gmx.de>
2017-05-12 18:22:16 +0200

    version number update script

--- setup/dsr-linux.spec
+++ setup/dsr-linux.spec
@@ -2,7 +2,7 @@ Summary: DSR - A program for modelling of disordered solvents with SHELXL
 Name: DSR
 Provides: DSR
 Packager: dkratzert@gmx.de
-Version: 203
+Version: 204
 Release: 0
 Requires: python, xclip
 Prefix: /opt
---
+++ setup/version_numbers.py
@@ -0,0 +1,57 @@
+#/usr/bin/env python
+#-*- encoding: utf-8 -*-
+
+from __future__ import print_function
+
+from dsr import VERSION
+import misc
+import dbfile
+import os
+
+"""
+This file is for updating the various version number definitions for each DSR release
+"""
+
+debianpath = os.path.abspath("./debian-package/DEBIAN/control")  # for the .deb file
+isspath = os.path.abspath("./dsr-install.iss")  # for the windows executable
+specpath = os.path.abspath("./dsr-linux.spec")  # for the .rpm file
+
+
+def process_debian_and_spec(filepath):
+    """
+    search for: Version: 203
+    replace with "Version: VERSION"
+    """
+    debfile = dbfile.read_file_data(filepath, with_comments=True)
+    for num, line in enumerate(debfile):
+        if line.startswith("Version:"):
+            l = line.split()
+            l[1] = VERSION
+            debfile[num] = " ".join(l)+"\n"
+            break
+    misc.write_file(debfile, filepath)
+
+
+def process_iss(filepath):
+    """
+    #define MyAppVersion  "203"
+    """
+    debfile = dbfile.read_file_data(filepath, with_comments=True)
+    for num, line in enumerate(debfile):
+        if line.startswith("#define MyAppVersion"):
+            l = line.split()
+            l[2] = '"{}"'.format(VERSION)
+            debfile[num] = " ".join(l)+"\n"
+            break
+    misc.write_file(debfile, filepath)
+
+
+if __name__ == "__main__":
+
+    process_debian_and_spec(debianpath)
+
+    process_debian_and_spec(specpath)
+
+    process_iss(isspath)
+
+
