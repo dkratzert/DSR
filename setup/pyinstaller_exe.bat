7f09c359f7036df6a8181fbcadf712da635c4e86
Daniel Kratzert <dkratzert@gmx.de>
2018-01-10 16:34:39 +0100

    pyinstaller script, but startup of DSR is slow with single exe

--- setup/pyinstaller_exe.bat
+++ setup/pyinstaller_exe.bat
@@ -1,2 +1,2 @@
 cd ..
-c:\tools\Python2.7.13_32\Scripts\pyinstaller.exe --clean --add-data="manuals;manuals" --add-data="example;example" -n DSR -w -F -y dsr.py
+c:\tools\Python2.7.13_32\Scripts\pyinstaller.exe --clean --add-data="manuals;manuals" --add-data="example;example" -n DSR -F -y dsr.py
