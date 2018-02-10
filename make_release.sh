689c10b7b508d991edb353a7dfd0c1d780dc5ba4
Daniel Kratzert <dkratzert@gmx.de>
2018-02-10 09:00:00 +0100

    more release script fixes

--- make_release.sh
+++ make_release.sh
@@ -1,22 +1,47 @@
+#!/usr/bin/env bash
 #/bin/bash
 
 GIT="$(pwd)"
 PACK="/usr/src/packages"
+
 cd $GIT
 
-#dos2unix *
-
-VERSION=$(cat $GIT/dsr.py|grep -e "VERSION ="|cut -d ' ' -f3|tr -d "\'")
-
+VERSION=$(cat ${GIT}/dsr.py|grep -e "VERSION ="|cut -d ' ' -f3|tr -d "\'")
+TMPDIR=/tmp/DSR-${VERSION}
+BUILDDIR=/usr/src/packages/BUILD/DSR-${VERSION}
+DEBDIR=/usr/src/packages/BUILD/dsr
 
 cd $GIT
 
 #cd setup
 #sh ./setup/build_linux_distrib.sh
-PYTHONPATH=$GIT
-python ./scripts/make_zipfile.py
+rm $GIT/setup/Output/DSR-${VERSION}.tar.gz
+PYTHONPATH=$GIT python ./scripts/make_zipfile.py
+cp setup/Output/DSR-${VERSION}.tar.gz /usr/src/packages/BUILD/
 
-cd $GIT
+cd ${GIT}
+
+############  Prepare debian package  ########################
+
+rm -rf /tmp/DSR-${VERSION}
+tar -xf setup/Output/DSR-$VERSION.tar.gz -C /tmp
+cp -r $TMPDIR/* $BUILDDIR
+# for debian:
+
+rm -r ${DEBDIR}
+mkdir $DEBDIR
+mkdir $DEBDIR/etc
+mkdir $DEBDIR/etc/profile.d
+mkdir $DEBDIR/opt
+mkdir $DEBDIR/opt/DSR
+cp $TMPDIR/* $DEBDIR/opt/DSR
+cp -r $TMPDIR/example $DEBDIR/opt/DSR
+cp -r $TMPDIR/manuals $DEBDIR/opt/DSR
+cp -r $TMPDIR/networkx $DEBDIR/opt/DSR
+cp -r $TMPDIR/mpmath $DEBDIR/opt/DSR
+cp $TMPDIR/setup/* $DEBDIR/etc/profile.d
+
+##########################################
 
 cp $GIT/setup/dsr-linux.spec $PACK/SPECS
 
@@ -32,7 +57,6 @@ rpmbuild -bb dsr-linux.spec -v
 echo -e "####### rpm ready #########\n" 
 
 cp /usr/src/packages/RPMS/noarch/DSR-$VERSION-0.noarch.rpm /usr/src/packages/BUILD/
-cp /usr/src/packages/SOURCES/DSR-$VERSION.tar.gz /usr/src/packages/BUILD/
 
 #exit 
 #######################################
