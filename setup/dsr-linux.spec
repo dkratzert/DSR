8fc076512af5ec1236c26b43377c0067d6fe82cb
Daniel Kratzert <dkratzert@gmx.de>
2018-11-28 14:36:18 +0100

    Version 218

--- setup/dsr-linux.spec
+++ setup/dsr-linux.spec
@@ -2,7 +2,7 @@ Summary: DSR - A program for modelling of disordered solvents with SHELXL
 Name: DSR
 Provides: DSR
 Packager: dkratzert@gmx.de
-Version: 217
+Version: 218
 Release: 0
 Requires: python, xclip
 Prefix: /opt
---
+++ setup/make_win_release.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+This script has to be run from the main dir e.g. D:\GitHub\StructureFinder
+"""
+import subprocess
+
+from dsr import VERSION
+from scripts.make_zipfile import make_zip, files
+from setup.version_numbers import process_debian_and_spec, process_iss, debianpath, specpath, isspath
+
+print("Updating version numbers to version {} ...".format(VERSION))
+
+print("Linux deb... {}".format(VERSION))
+process_debian_and_spec(debianpath)
+
+print("Linux rpm... {}".format(VERSION))
+process_debian_and_spec(specpath)
+
+print("windows iss... {}".format(VERSION))
+process_iss(isspath)
+
+print("Version numbers updated.")
+
+
+def make_distribs():
+    innosetup_compiler = r'D:/Program Files (x86)/Inno Setup 5/ISCC.exe'
+    # Run DSR setup compiler
+    subprocess.call([innosetup_compiler, r'D:\GitHub\DSR\setup\dsr-install.iss', ])
+
+
+# Make binary distributions:
+make_distribs()
+
+# Make a zip file for web interface distribution:
+make_zip(files)
