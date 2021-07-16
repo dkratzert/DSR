4aaed3640af62373f89339a53792fc97a9f2671e
Daniel Kratzert <dkratzert@gmx.de>
2021-07-16 16:19:44 +0200

    small fix in release script

---
+++ setup/make_win_release.bat
@@ -0,0 +1,2 @@
+cd ..
+c:\Python27-dsr\python.exe setup\make_win_release.py
--- setup/make_win_release.py
+++ setup/make_win_release.py
@@ -30,7 +30,7 @@ print("Version numbers updated.")
 def make_distribs():
     innosetup_compiler = r'C:\Program Files (x86)\Inno Setup 6/ISCC.exe'
     # Run DSR setup compiler
-    subprocess.call([innosetup_compiler, r'D:\GitHub\DSR\setup\dsr-install.iss', ])
+    subprocess.call([innosetup_compiler, r'setup\dsr-install.iss', ])
 
 
 # Make a zip file for web interface distribution:
