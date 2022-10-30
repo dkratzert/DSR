24b87f2f8e1eef25fe76878f53d3531f06947a2b
Daniel Kratzert <dkratzert@gmx.de>
2022-10-30 19:31:49 +0100

    project files

--- src/dsr/fit/quatfit.py
+++ src/dsr/fit/quatfit.py
@@ -1,8 +1,7 @@
 # coding=utf-8
 
 import copy
-import sys
-sys.path.append('../')
+
 from math import fabs, sqrt
 from misc import cart_to_frac, frac_to_cart, matrix_minus_vect, matrix_plus_vect, transpose
 
