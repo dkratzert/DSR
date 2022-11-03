a56c962055049ff25f504d879e1c9ad40b0f846b
Daniel Kratzert <dkratzert@gmx.de>
2022-11-03 22:26:42 +0100

    add start script for pip package

---
+++ src/dsr/__init__.py
@@ -0,0 +1,8 @@
+import os
+import sys, pathlib
+
+pth = pathlib.Path(__file__).parent
+sys.path.insert(0, str(pth))
+os.environ["DSR_DIR"] = str(pth)
+# print(pth)
+#echo export PATH="str(pth)" >> ~/.profile
